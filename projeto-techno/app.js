3c561be6f8bcb2e53a0fca2bc6e97c885527ed4f
Matheus Ribeiro <matheusbrdev@gmail.com>
2021-02-09 14:33:49 -0300

    Class about modal

--- projeto-techno/app.js
+++ projeto-techno/app.js
@@ -1,7 +1,8 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    products: [], 
+    products: [],
+    singleProduct: false,
   },
   methods: {
     pullTheProducts() {
@@ -11,8 +12,36 @@ const vm = new Vue({
           this.products = json;
         });
     },
+    pullSingleProduct(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((response) => response.json())
+        .then((json) => {
+          this.singleProduct = json;
+        });
+    },
+    openModal(id) {
+      this.pullSingleProduct(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    closeModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.singleProduct = false;
+      }
+    },
   },
   created() {
     this.pullTheProducts();
+  },
+  filters: {
+    //para tratar os dados de preço
+    priceNumber(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
   },
 });
